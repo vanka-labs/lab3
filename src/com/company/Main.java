e83cbd74f10f3685b7d32d1c20f50ecaa289eb68
vanka-labs <75120231+vanka-labs@users.noreply.github.com>
2020-12-17 19:20:08 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,16 @@ import java.io.File;
 import java.io.IOException;
 
 
+
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+   // static void check(int[][] a) {
+       //for (int i = 0; i < n; ++i) {
+          //  if (a[i][i] == 0) {
+            //    throw new NullPointerException("Can't solve");
+            //}
+        //}
+    //}
+    public static void main(String[] args) throws FileNotFoundException{
 
         int c = 0;
         int n = 3;
@@ -18,12 +26,18 @@ public class Main {
         int[] b = new int[n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                a[i][j] = scanner.nextInt();
+                a[i][j] = 0;
             }
-            b[i] = scanner.nextInt();
+            b[i] = 0;
         }
 
-        for (int i = 0; i < n; ++i) {
+         for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = scanner.nextInt();
+                }
+                b[i] = scanner.nextInt();
+            }
+         for (int i = 0; i < n; ++i) {
             System.out.println();
             for (int j = 0; j < n; ++j) {
               System.out.print(a[i][j] + "\t");
@@ -32,23 +46,37 @@ public class Main {
            System.out.print(b[i] + "\t");
         }
 
+
 //Gaus
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = n - 1; j >= 1; --j) {
-                if (i != j - 1) {
-                b[i] /= a[i][i];
-                    a[i][i] = 1;
-                    c = a[j - 1][i];
-                    a[j - 1][i] -= a[i][i] * c;
-                    b[j - 1] = b[j - 1] - b[i] * c;
+
+            for (int i = n - 1; i >= 0; --i) {
+                for (int j = n - 1; j >= 1; --j) {
+                    if (i != j - 1) {
+                      if(a[i][i]==0) {
+                        throw new ArithmeticException("Can't solve");
+                      }
+                      else if(a[i][i]!=0) {
+                          b[i] /= a[i][i];
+
+                          a[i][i] = 1;
+                          c = a[j - 1][i];
+                          a[j - 1][i] -= a[i][i] * c;
+                          b[j - 1] = b[j - 1] - b[i] * c;
+                      }
+                    }
                 }
             }
-        }
-        System.out.println("\n"+"\n");
-        for (int i = 0; i < n; ++i) {
-            x[i] = b[i] / a[i][i];
-            System.out.print(x[i] + "\t");
-        }
+            System.out.println("\n" + "\n");
+            for (int i = 0; i < n; ++i) {
+                if(a[i][i]==0) {
+                    throw new ArithmeticException("Can't solve");
+                }
+                else if(a[i][i]!=0) {
+                    x[i] = b[i] / a[i][i];
+                }
+                System.out.print(x[i] + "\t");
+            }
+
 
         System.out.println("\n"+"\n");
 
@@ -79,6 +107,10 @@ public class Main {
 
         for (int k = 0; k < n; k++)
         {
+            if(A[k][k]==0) {
+                throw new NullPointerException("Can't solve");
+            }
+            else if(A[k][k]!=0){
             temp = A[k][k];
 
             for (int j = 0; j < n; j++)
@@ -86,6 +118,7 @@ public class Main {
                 A[k][j] /= temp;
                 E[k][j] /= temp;
             }
+
 
             for (int i = k + 1; i < n; i++)
             {
@@ -97,6 +130,7 @@ public class Main {
                     E[i][j] -= E[k][j] * temp;
                 }
             }
+        }
         }
 
         for (int k = n - 1; k > 0; k--)
